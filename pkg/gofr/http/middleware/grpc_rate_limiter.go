36c6ee1ccb690c8579832bbecc2426273b48f296
0xEbrahim <elsayedebrahim603@gmail.com>
2025-11-21 14:49:06 +0200

    feat: added gRPC unary server interceptor

--- pkg/gofr/http/middleware/grpc_rate_limiter.go
+++ pkg/gofr/http/middleware/grpc_rate_limiter.go
@@ -2,11 +2,16 @@ package middleware
 
 import (
 	"context"
+	"net"
 
 	"sync"
 	"time"
 
 	"golang.org/x/time/rate"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
 )
 
 type GRPCRateLimiterConfig struct {
@@ -69,5 +74,32 @@ func (rl *grpcRateLimiter) cleanupStaleEntries() {
 			}
 			return true
 		})
+	}
+}
+
+func getGRPCIP(ctx context.Context) string {
+	ip := "unknown"
+
+	if p, ok := peer.FromContext(ctx); ok && p.Addr != nil {
+		ip, _, _ = net.SplitHostPort(p.Addr.String())
+	}
+
+	return ip
+}
+
+func (rl *grpcRateLimiter) Unary() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		ip := getGRPCIP(ctx)
+		limiter := rl.getLimiter(ip)
+
+		if !limiter.Allow() {
+			if rl.metrics != nil {
+				rl.metrics.IncrementCounter(ctx, "app_grpc_rate_limit_exceeded_total",
+					"method", info.FullMethod, "ip", ip)
+			}
+			return nil, status.Error(codes.ResourceExhausted, "rate limit exceeded")
+		}
+
+		return handler(ctx, req)
 	}
 }
